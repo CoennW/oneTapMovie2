21b91522236b05ce54f49f56fd56254c329e687c
Micromize1 <coen_walter@hotmail.com>
2017-06-10 14:30:41 +0200

    NEW PROJECT NO ERRORS

---
+++ OneTapMovie2/SearchPageViewController.swift
@@ -0,0 +1,145 @@
+//
+//  SearchPageViewController.swift
+//  OneTapMovie2
+//
+//  Created by Fhict on 10/06/2017.
+//  Copyright © 2017 Fhict. All rights reserved.
+//
+
+import UIKit
+import TMDBSwift
+
+class SearchPageViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    //textfield outlets
+    @IBOutlet weak var fromYearField: UITextField!
+    @IBOutlet weak var tillYearField: UITextField!
+    @IBOutlet weak var genreField: UITextField!
+    @IBOutlet weak var durationField: UITextField!
+    @IBOutlet weak var totalVotesField: UITextField!
+    @IBOutlet weak var voteAvarageField: UITextField!
+    
+    //pickers declaring
+    let fromYearPicker = UIPickerView()
+    let tillYearPicker = UIPickerView()
+    let genrePicker = UIPickerView()
+    let durationPicker = UIPickerView()
+    
+    //data for pickers
+    var genreArray = [String]()
+    var yearsArray = ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010" ,"2011", "", "2012", "2013", "2014" ,"2015" ,"2016" ,"2017"]
+    
+    //data for search and passing(other controllers)
+    var selectedFromYear = String()
+    var selectedTillYear = String()
+    var selectedGenre = String()
+    var selectedDuration = String()
+    var selectedTotalVotes = String()
+    var selectedVoteAvarage = String()
+    
+    //api key for The Movie Database
+    let apiKey = "279e6330590f5e8788be345bf87321ca"
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //set inputfield to correct state
+        setInputFields()
+        fillPickers()
+        //set data to pickerviews 
+        
+        //Pickerview met jaren 
+        //Pickerview met genres 
+        //keyboard als numberpad met total/avarage votes
+        
+    }
+    
+    func setInputFields() {
+        
+        //set pickerview
+        genrePicker.delegate = self
+        fromYearPicker.delegate = self
+        tillYearPicker.delegate = self
+        genrePicker.tag = 1
+        fromYearPicker.tag = 2
+        tillYearPicker.tag = 3
+        
+        //set keyboard to numberpad 
+        totalVotesField.keyboardType = UIKeyboardType.numberPad
+        voteAvarageField.keyboardType = UIKeyboardType.numberPad
+        
+        //set textfields input to picker
+        genreField.inputView = genrePicker
+        fromYearField.inputView = fromYearPicker
+        tillYearField.inputView = tillYearPicker
+        
+    
+    }
+    
+    func fillPickers() {
+        //fill genrePicker
+        GenresMDB.genres(apiKey, listType: .movie, language: "en") { (apiReturn, genres) in
+            if let genres = genres{
+                genres.forEach{
+                    print($0.name!)
+                    self.genreArray.append($0.name!)
+                }
+            }
+        }
+
+    
+    }
+    
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 1
+    }
+
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        if pickerView.tag == 1 {
+            return genreArray.count
+        }else if pickerView.tag == 2 || pickerView.tag == 3 {
+            return yearsArray.count
+        }
+        return 0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView.tag == 1 {
+            return genreArray[row]
+        }else if pickerView.tag == 2 || pickerView.tag == 3 {
+            return yearsArray[row]
+        }
+        return nil
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView.tag == 1 {
+            genreField.text = genreArray[row]
+        }else if pickerView.tag == 2 {
+            fromYearField.text = yearsArray[row]
+        }else if pickerView.tag == 3 {
+            tillYearField.text = yearsArray[row]
+        }
+    }
+   
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'OneTapMovie2' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for OneTapMovie2
+pod ‘TMDBSwift’
+
+  target 'OneTapMovie2Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'OneTapMovie2UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
