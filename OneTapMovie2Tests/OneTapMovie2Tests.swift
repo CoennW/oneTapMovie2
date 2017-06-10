7159918c11ad777647d231e568f2de7b2b7308ca
Micromize1 <coen_walter@hotmail.com>
2017-06-10 13:22:06 +0200

    Initial Commit

---
+++ OneTapMovie2Tests/OneTapMovie2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  OneTapMovie2Tests.swift
+//  OneTapMovie2Tests
+//
+//  Created by Fhict on 10/06/2017.
+//  Copyright © 2017 Fhict. All rights reserved.
+//
+
+import XCTest
+@testable import OneTapMovie2
+
+class OneTapMovie2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
