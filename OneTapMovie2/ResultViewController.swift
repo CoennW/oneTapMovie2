bdd7776e5e517884bb492d738847a7961004d3b6
Micromize1 <coen_walter@hotmail.com>
2017-06-11 13:49:46 +0200

    result almost rdy

--- OneTapMovie2/ResultViewController.swift
+++ OneTapMovie2/ResultViewController.swift
@@ -11,43 +11,107 @@ import TMDBSwift
 
 class ResultViewController: UIViewController {
     
-    //Api key
-    let apiKey = ""
+    //Api key if needed
+    let apiKey = "279e6330590f5e8788be345bf87321ca"
+    
+    //base poster url
+    let basePosterURL = "https://image.tmdb.org/t/p/w500/"
+    
+    //movieID 
+    var movieID = Int()
     
     //get outlets
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var ratingLabel: UILabel!
-    @IBOutlet weak var directedByLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
     @IBOutlet weak var posterImage: UIImageView!
-    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
     @IBOutlet weak var overviewTextField: UITextView!
     
-    //Movie array
+    //Movie array for selected movie
     var selectedMovieArray = [MovieMDB]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        movieID = selectedMovieArray[0].id!
         fillViewController()
         getMovieDetail()
-        // Do any additional setup after loading the view.
+        
     }
     
     func getMovieDetail() {
         
+        MovieMDB.credits(apiKey, movieID: movieID){
+            apiReturn, credits in
+            
+            /*if let credits = credits{
+                for crew in credits.crew{
+                    print(crew.job)
+                    print(crew.name)
+                }
+            }*/
+            
+            var director = String()
+            if let credits = credits {
+                for crew in credits.crew {
+                    if crew.job == "Director" {
+                        director = crew.name
+                        print(director)
+                        break
+                    }
+                }
+            }
+           
+        }
         
-        ratingLabel.text = String(describing: selectedMovieArray[0].id)
     }
     
+
+    
     func fillViewController(){
+        //get data from movieArray and fill labels
         titleLabel.text = selectedMovieArray[0].title
-        directedByLabel.text = String(describing: selectedMovieArray[0].vote_average)
-        ratingLabel.text = String(describing: selectedMovieArray[0].vote_count)
+        releaseDateLabel.text = String(describing: selectedMovieArray[0].release_date!)
+        ratingLabel.text = String(describing: selectedMovieArray[0].vote_average!)
         overviewTextField.text = String(describing: selectedMovieArray[0].overview!)
+        languageLabel.text = selectedMovieArray[0].original_language!
+        
+        //download poster and put in imageview(probeer te a-syncen)
+        let posterPath = basePosterURL + selectedMovieArray[0].poster_path!
+        print(posterPath)
+        
+        DispatchQueue.global().async {
+         let imageUrl:URL = URL(string: posterPath)!
+         let imageData:NSData = NSData(contentsOf: imageUrl)!
+        
+         DispatchQueue.main.async {
+         let image = UIImage(data: imageData as Data)
+         self.posterImage.image = image
+         }
+         
+         }
+
+        
+        
         
         
         
     }
 
+    @IBAction func searchAgainTapped(_ sender: Any) {
+        
+        performSegue(withIdentifier: "searchAgain", sender: self)
+        
+    }
+    
+    @IBAction func watchThisMovieTapped(_ sender: Any) {
+        
+        
+        performSegue(withIdentifier: "watchingMovie", sender: self)
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
